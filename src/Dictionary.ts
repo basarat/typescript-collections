034afa193965101efd9738bbcc2d5ed75735279e
CaselIT <cfederico87@gmail.com>
2016-01-30 11:34:44 +0100

    Removed unnecessary toString to avoid the typerrors

--- src/Dictionary.ts
+++ src/Dictionary.ts
@@ -211,7 +211,7 @@ export default class Dictionary<K, V>{
     toString(): string {
         let toret = "{";
         this.forEach((k, v) => {
-            toret = toret + "\n\t" + (k === null ? k : k.toString()) + " : " + (v === null ? v : v.toString())
+            toret += `\n\t${k} : ${v}`;
         });
         return toret + "\n}";
     }
